bf2d950de92013c116dc22aeb0ce5aa220db4b90
mors6960@gmail.com <mors6960@gmail.com>
2024-07-03 12:04:53 +0530

    removing server

--- server/routes/botRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { handleMessage } = require('../controllers/botController');
-
-const router = express.Router();
-
-router.post('/webhook', handleMessage);
-
-module.exports = router;
